8bc1bce3badb12eacba468d2b51918d2668366ae
Carlos Cañellas <ratiosu.cct@gmail.com>
2024-11-26 08:17:57 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env sh
 
-docker build . -t lifecycle_lib:latest
 docker run \
     --rm -it \
     -v $PROJECT_DIR:/build \
     -v /var/run/docker.sock:/var/run/docker.sock \
-    lifecycle_lib:latest ci build -t test_container
+    test_container:latest ci build -t test_container:latest
