94994a8641067380f7e55f4dea3d443d1e28b0a4
Carlos Cañellas <ratiosu.cct@gmail.com>
2024-11-26 08:30:23 +0100

    Update docker.py

--- ci/library/docker.py
+++ ci/library/docker.py
@@ -3,19 +3,18 @@ import logging
 from rich.logging import RichHandler
 
 FORMAT = "%(message)s"
-logging.basicConfig(
-    level="NOTSET", format=FORMAT, datefmt="[%X]", handlers=[RichHandler()]
-)
 
-log = logging.getLogger("rich")
 
 class DockerLib:
-    def __init__(self) -> None:
+    def __init__(self, logLevel) -> None:
         self.client = docker.client.from_env()
-
+        logging.basicConfig(
+            level=loglevel, format=FORMAT, datefmt="[%X]", handlers=[RichHandler()]
+        )
+        self.log = logging.getLogger("rich")
 
     def build(self, tag):
-        log.info("Starting container build...")
+        self.self.log.info("Starting container build...")
         problems = []
         try:
             image, logs = self.client.images.build(path="/build", tag=tag, timeout=900)
@@ -23,16 +22,15 @@ class DockerLib:
                 for chunk in logs:
                     if 'stream' in chunk:
                         for line in chunk['stream'].splitlines():
-                            log.debug(line)
+                            self.log.debug(line)
         except docker.errors.BuildError as e:
             problems.append(e)
         finally:
             if len(problems) > 0:
-                log.error("Errors while building the container:")
-                [log.error(problem) for problem in problems]
+                self.log.error("Errors while building the container:")
+                [self.log.error(problem) for problem in problems]
             else:
-                log.info("Container built successfully!")
-
+                self.log.info("Container built successfully!")
 
     def prune(self):
         return self.client.images.prune_builds()
